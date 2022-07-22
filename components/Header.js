9b2237996509b39cd766d43a0449285c7cb17e21
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2022-07-22 09:45:33 -0500

    createUser and swapTokens components

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import logo from '../assets/logo.png'
 import Image from 'next/image'
 import { AiOutlineSearch } from 'react-icons/ai'
-
+import { RobinhoodContext } from '../context/RobinhoodContext'
 
 const styles = {
     container: 'flex w-screen h-16 bg-black px-24 py-3 mb-5 fixed',
-    LeftHeader: 'flex flex-1',
-    Logo: 'object-cover cursor-pointer',
+    leftHeader: 'flex flex-1',
+    logo: 'object-cover cursor-pointer',
     searchWrapper: 'flex flex-1',
-    searchInputContainer: 'text-white items-center flex flex-1 ml-64 border border-gray-400 mr-64 hover:bg-[#1E2123] duration-300 p-3 rounded-lg',
+    searchInputContainer:
+        'text-white items-center flex  flex-1 -ml-64 border border-gray-400 mr-64 hover:bg-[#1E2123] duration-300 p-3 rounded-lg',
     searchIcon: 'text-gray-400 text-3xl mr-3',
     searchInputWrapper: 'text-gray-400 text-lg w-full',
     searchInput: 'bg-transparent outline-none w-full',
     rightHeader: 'flex items-center justify-end text-white gap-8',
-    menuItem: 'cursor-pointer font-bold hover:text-green-500 duration-300'
+    menuItem: 'cursor-pointer font-bold hover:text-green-500 duration-300',
 }
 
-// Make dynamic later
-const isAuthenticated = true
-const formattedAccount = '0xcd...1E4D'
-
 const Header = () => {
+    const {
+        connectWallet,
+        signOut,
+        currentAccount,
+        isAuthenticated,
+        formattedAccount,
+        swapTokens,
+    } = useContext(RobinhoodContext)
+
     return (
         <div className={styles.container}>
-            <div className={styles.LeftHeader}>
-                {/* <Image /> */}
+            <div className={styles.leftHeader}>
+                <Image src={logo} height={100} width={100} className={styles.logo} />
             </div>
             <div className={styles.searchWrapper}>
                 <div className={styles.searchInputContainer}>
@@ -35,21 +42,28 @@ const Header = () => {
                 </div>
             </div>
             <div className={styles.rightHeader}>
-                <div className={styles.menuItem}>Rewards</div>
+                <div className={styles.menuItem} onClick={swapTokens}>
+                    Rewards
+                </div>
                 <div className={styles.menuItem}>Portfolio</div>
                 <div className={styles.menuItem}>Cash</div>
                 <div className={styles.menuItem}>Messages</div>
                 {isAuthenticated && (
                     <>
-                        <div className={styles.menuItem}>format</div>
-                        <div className={styles.menuItem} onClick={() => connectWallet()}>Logout</div>
+                        <div className={styles.menuItem}>{formattedAccount}</div>
+                        <div className={styles.menuItem} onClick={() => signOut()}>
+                            Logout
+                        </div>
                     </>
                 )}
+
                 {!isAuthenticated && (
-                    <div className={styles.menuItem} onClick={() => connectWallet()}>Login</div>
+                    <div className={styles.menuItem} onClick={() => connectWallet()}>
+                        Login
+                    </div>
                 )}
             </div>
-        </div >
+        </div>
     )
 }
 
